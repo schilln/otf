f99d58f012c3e5d6b412d796b20717aeef963bc0
Nathan Schill <nateschill@gmail.com>
2025-09-22 10:27:43 -0600

    extracted plotting code to separate file

---
+++ examples/lorenz63/plot.py
@@ -0,0 +1,40 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def plot(c1s, c2s, c3s, u_errors, g1, g2, g3, tls):
+    num_iters = len(c1s)
+    ils = np.arange(num_iters)
+
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    ax = axs[0, 0]
+    ax.hlines(g1, ils[0], ils[-1], label="g1", color="black")
+    ax.plot(ils, c1s, label="c1")
+    ax.legend()
+    ax.set_title("c1 vs g1")
+    ax.set_xlabel("Iteration number")
+
+    ax = axs[0, 1]
+    ax.hlines(g2, ils[0], ils[-1], label="g2", color="black")
+    ax.plot(ils, c2s, label="c2")
+    ax.legend()
+    ax.set_title("c2 vs g2")
+    ax.set_xlabel("Iteration number")
+
+    ax = axs[1, 0]
+    ax.hlines(g3, ils[0], ils[-1], label="g3", color="black")
+    ax.plot(ils, c3s, label="c3")
+    ax.legend()
+    ax.set_title("c3 vs g3")
+    ax.set_xlabel("Iteration number")
+
+    ax = axs[1, 1]
+    ax.plot(tls[1:], u_errors)
+    ax.set_yscale("log")
+    ax.set_title("Relative error in $u$")
+    ax.set_xlabel("Time")
+
+    fig.tight_layout()
+
+    return fig, axs
