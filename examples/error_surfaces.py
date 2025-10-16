cf7ab8c9cbf78c0394bc56732ac9147fc9c2675c
Nathan Schill <nateschill@gmail.com>
2025-10-15 22:39:43 -0600

    added functionality to compute trajectory of parameter updates

--- examples/error_surfaces.py
+++ examples/error_surfaces.py
@@ -30,8 +30,15 @@ def get_errors(
     yn: int = 11,
     x_relative_bound: float = 1,
     y_relative_bound: float = 1,
-) -> tuple[jndarray, ndarray, ndarray]:
-    """
+    start_relative_position: tuple[int, int] | None = None,
+    optimizer_type: type[optim.base.BaseOptimizer] | None = None,
+    optimizer_kwargs: dict = dict(),
+    sim_T0: float | None = 0,
+    sim_Tf: float | None = None,
+    sim_t_relax: float | None = 1,
+) -> dict:
+    """Compute an error surface. Optionally compute a trajectory of parameter
+    updates along the surface if `start_relative_position` is not None.
 
     Parameters
     ----------
@@ -42,7 +49,63 @@ def get_errors(
         i.e., simulate on grid of shape (xn, yn)
     x_relative_bound, y_relative_bound
         Maximum relative step size in each random direction
+    start_relative_position
+        If not None, used to compute starting parameter values from the error
+        surface domain relative to (`x_relative_bound`, `y_relative_bound`),
+        then simulate a system using `true_observed` and `optimizer` to update
+        parameters. Include the sequence of coordinates of the updated
+        parameters in the returned `(xls, yls)` domain.
+    optimizer_type
+        Optimizer type to use to update parameters if `start_relative_position`
+        is not None.
+    optimizer_options
+        Keyword arguments with which to initialize `optimizer_type`
+    sim_T0
+        Initial time at which to begin simulation if `start_relative_position`
+        is not None
+    sim_Tf
+        (Approximate) final time for simulation if `start_relative_position` is
+        not None
+    sim_t_relax
+        (Approximate) length of time to simulate system between parameter
+        updates if `start_relative_position` is not None
+
+    Returns
+    -------
+    result
+        Dictionary containing the following keys:
+
+        - **errors** (*jndarray*) - Grid of relative errors, shape (yn, xn,
+          len(true_observed))
+        - **xls** (*ndarray*) - x-coordinates of grid, shape (xn,)
+        - **yls** (*ndarray*) - y-coordinates of grid, shape (yn,)
+        - **cs** (*ndarray*, optional) - Sequence of parameter values during
+          optimization. Only present if `start_relative_position` is not None.
+        - **cs_coordinates** (*ndarray*, optional) - Sequence of positions of
+          `cs` values in `(xls, yls)` domain using updates provided by
+          `optimizer`, shape (N, 2) where N is number of iterations. Only
+          present if `start_relative_position` is not None.
     """
+    if start_relative_position is not None:
+        if optimizer_type is None:
+            raise ValueError(
+                "`optimizer` must be provided if `start_relative_position`"
+                " is not None"
+            )
+        if len(start_relative_position) != 2:
+            raise ValueError(
+                "`start_relative_position` must contain two elements"
+            )
+        for item in start_relative_position:
+            if not isinstance(item, (int, float)):
+                raise ValueError(
+                    "elements of `start_relative_position` must be numbers"
+                )
+        if not isinstance(sim_T0, (int, float)) or not isinstance(
+            sim_Tf, (int, float)
+        ):
+            raise ValueError("`sim_T0` and `sim_Tf` must be numbers")
+
     x_relative_bound = abs(x_relative_bound)
     y_relative_bound = abs(y_relative_bound)
 
@@ -86,7 +149,38 @@ def get_errors(
 
         errors[yi, xi] = u_errors
 
-    return errors, xls, yls
+    if start_relative_position is None:
+        return {"errors": errors, "xls": xls, "yls": yls}
+
+    system.cs = (
+        cs_center
+        + start_relative_position[0] * dirs[0]
+        + start_relative_position[1] * dirs[1]
+    )
+
+    optimizer = optimizer_type(system, **optimizer_kwargs)
+    cs, *_ = utils.run_update(
+        system,
+        true_observed,
+        assimilated_solver,
+        dt,
+        sim_T0,
+        sim_Tf,
+        sim_t_relax,
+        assimilated0,
+        optimizer=optimizer,
+        return_all=False,
+        true_actual=true_actual,
+    )
+    cs_coordinates = np.linalg.lstsq(dirs.T, (cs - cs_center).T)[0].T
+
+    return {
+        "errors": errors,
+        "xls": xls,
+        "yls": yls,
+        "cs": cs,
+        "cs_coordinates": cs_coordinates,
+    }
 
 
 def plot(fig, ax, errors: jndarray, xls: ndarray, yls: ndarray):
