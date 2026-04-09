e42771bb514f04ac257a6a410c4b5356d3b99abb
Nathan Schill <nateschill@gmail.com>
2026-04-09 12:10:55 -0600

    exposed LinearNonlinear classes

--- src/otf/system/__init__.py
+++ src/otf/system/__init__.py
@@ -1,4 +1,15 @@
 from . import utils
 from .base import BaseSystem, System_ModelKnown, System_ModelUnknown
+from .linear_nonlinear import (
+    System_LinearNonlinear_ModelKnown,
+    System_LinearNonlinear_ModelUnknown,
+)
 
-__all__ = ["utils", "BaseSystem", "System_ModelUnknown", "System_ModelKnown"]
+__all__ = [
+    "utils",
+    "BaseSystem",
+    "System_ModelUnknown",
+    "System_ModelKnown",
+    "System_LinearNonlinear_ModelKnown",
+    "System_LinearNonlinear_ModelUnknown",
+]
