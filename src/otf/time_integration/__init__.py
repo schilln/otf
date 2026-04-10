eb5e4887e535a938b74998e70b8eb01107ebb589
Nathan Schill <nateschill@gmail.com>
2026-04-10 16:18:55 -0600

    exposed linear-nonlinear solvers

--- src/otf/time_integration/__init__.py
+++ src/otf/time_integration/__init__.py
@@ -1,3 +1,9 @@
+from .linear_nonlinear import (
+    AB2AM2,
+    AB2BD2,
+    ETD1,
+    ETD2,
+)
 from .solver import (
     RK4,
     ForwardEuler,
@@ -7,6 +13,10 @@ from .solver import (
 )
 
 __all__ = [
+    "AB2AM2",
+    "AB2BD2",
+    "ETD1",
+    "ETD2",
     "RK4",
     "ForwardEuler",
     "FourStepAdamsBashforth",
