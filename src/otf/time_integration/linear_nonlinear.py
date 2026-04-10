99cd6e859a9a0f6584a433ed37fb365c92d0be0c
Nathan Schill <nateschill@gmail.com>
2026-04-10 16:16:17 -0600

    implemented AB2AM2 and AB2BD2

--- src/otf/time_integration/linear_nonlinear.py
+++ src/otf/time_integration/linear_nonlinear.py
@@ -108,3 +108,93 @@ class ETD2(MultistepSolver):
             return assimilated, (dt, cs, true_observed)
 
         return step_true, step_assimilated
+
+
+class AB2AM2(MultistepSolver):
+    _k = 2
+
+    def _step_factory(self):
+        def step_true(i, vals):
+            l, nl_fn = self.system.linear_true(), self.system.nonlinear_true_ode
+
+            true, (dt,) = vals
+            t2 = true[i - 2]
+            t1 = true[i - 1]
+
+            nl2 = nl_fn(t2)
+            nl1 = nl_fn(t1)
+
+            true = true.at[i].set(
+                ((1 + dt / 2 * l) * t1 + 3 * dt / 2 * nl1 - dt / 2 * nl2)
+                / (1 - dt / 2 * l)
+            )
+
+            return true, (dt,)
+
+        def step_assimilated(i, vals):
+            assimilated, (dt, cs, true_observed) = vals
+
+            l, nl_fn = (
+                self.system.linear_assimilated(cs),
+                self.system.nudged_nonlinear_assimilated_ode,
+            )
+            t2 = true_observed[i - 2]
+            t1 = true_observed[i - 1]
+            a2 = assimilated[i - 2]
+            a1 = assimilated[i - 1]
+
+            nl2 = nl_fn(cs, t2, a2)
+            nl1 = nl_fn(cs, t1, a1)
+
+            assimilated = assimilated.at[i].set(
+                ((1 + dt / 2 * l) * a1 + 3 * dt / 2 * nl1 - dt / 2 * nl2)
+                / (1 - dt / 2 * l)
+            )
+
+            return assimilated, (dt, cs, true_observed)
+
+        return step_true, step_assimilated
+
+
+class AB2BD2(MultistepSolver):
+    _k = 2
+
+    def _step_factory(self):
+        def step_true(i, vals):
+            l, nl_fn = self.system.linear_true(), self.system.nonlinear_true_ode
+
+            true, (dt,) = vals
+            t2 = true[i - 2]
+            t1 = true[i - 1]
+
+            nl2 = nl_fn(t2)
+            nl1 = nl_fn(t1)
+
+            true = true.at[i].set(
+                (4 * t1 - t2 + 4 * dt * nl1 - 2 * dt * nl2) / (3 - 2 * dt * l)
+            )
+
+            return true, (dt,)
+
+        def step_assimilated(i, vals):
+            assimilated, (dt, cs, true_observed) = vals
+
+            l, nl_fn = (
+                self.system.linear_assimilated(cs),
+                self.system.nudged_nonlinear_assimilated_ode,
+            )
+            t2 = true_observed[i - 2]
+            t1 = true_observed[i - 1]
+            a2 = assimilated[i - 2]
+            a1 = assimilated[i - 1]
+
+            nl2 = nl_fn(cs, t2, a2)
+            nl1 = nl_fn(cs, t1, a1)
+
+            assimilated = assimilated.at[i].set(
+                (4 * a1 - a2 + 4 * dt * nl1 - 2 * dt * nl2) / (3 - 2 * dt * l)
+            )
+
+            return assimilated, (dt, cs, true_observed)
+
+        return step_true, step_assimilated
